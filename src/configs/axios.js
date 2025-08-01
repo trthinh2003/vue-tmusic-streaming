c548d8257da10b9bde6a2fe7be8b50fb61293b23
thinhschool1357 <104267661+trthinh2003@users.noreply.github.com>
2025-08-01 20:53:46 +0700

    TMusicStreaming Web V1 Done

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:5298/api',
     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-    // baseURL: 'https://tmusic-streaming-net.onrender.com/api', //Môi trường production
     headers: {
         'Content-Type': 'application/json',
     },
